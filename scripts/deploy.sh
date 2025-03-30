ec155d2ccd87b2ed56c2e8be82104fd0a5914896
Lucas Leone <lucasmleone@gmail.com>
2025-03-30 00:04:57 -0300

    fix if problem

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ echo "Estoy en: $(pwd)"
 
 python3 src/app.py & sleep 1
 
-if [[ ps aux | grep -v grep | grep "python3 src/app.py" ]]; then
+if  ps aux | grep -v grep | grep "python3 src/app.py" ; then
     echo "O.O Service is running"
 else
     echo ">.< Service is not running"
