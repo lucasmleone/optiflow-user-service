c95a51cce748372830f4c75caa66484df759ca02
Lucas Leone <lucasmleone@gmail.com>
2025-03-29 23:58:31 -0300

    changing python >python3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@
 
 echo "Deploying service"
 
-python ../src/app.py & sleep 1
+python3 ../src/app.py & sleep 1
 
-if [[ ps aux | grep -v grep | grep "python ../src/app.py" ]]; then
+if [[ ps aux | grep -v grep | grep "python3 ../src/app.py" ]]; then
     echo "O.O Service is running"
 else
     echo ">.< Service is not running"
