5e51dd8eee9688c971ce56d96f40918d94415adb
Lucas Leone <lucasmleone@gmail.com>
2025-03-29 23:14:53 -0300

    deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ echo "Deploying service"
 python ../src/app.py & sleep 1
 
 if [[ ps aux | grep -v grep | grep "python ../src/app.py" ]]; then
-    echo "Service is running"
+    echo "O.O Service is running"
 else
-    echo "Service is not running"
+    echo ">.< Service is not running"
     exit 1
 fi
