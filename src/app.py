ed2d7ddb7f71135d7734d7b26ca286be2a2b791b
Lucas Leone <lucasmleone@gmail.com>
2025-03-30 00:07:02 -0300

    py service too short

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 # Simulación básica de un servicio para usuarios
+import time
+
 def main():
     print("Servicio Usuarios - OptiFlow corriendo...")
+    time.sleep(60)
 
 if __name__ == "__main__":
     main()
