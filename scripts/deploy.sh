73742435990ab23363c1ea3f150625af7ad8600c
Lucas Leone <lucasmleone@gmail.com>
2025-03-30 00:03:23 -0300

    ruta correcta de proyecto

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Deploying service"
+echo "Estoy en: $(pwd)"
 
-python3 ../src/app.py & sleep 1
+python3 src/app.py & sleep 1
 
-if [[ ps aux | grep -v grep | grep "python3 ../src/app.py" ]]; then
+if [[ ps aux | grep -v grep | grep "python3 src/app.py" ]]; then
     echo "O.O Service is running"
 else
     echo ">.< Service is not running"
