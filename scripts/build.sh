e91d7f1c52fd2e4d071de17663ee12c2f158cc39
Lucas Leone <lucasmleone@gmail.com>
2025-03-29 23:51:09 -0300

    adding python lib

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "iniciando servicio de usuario"
 sleep 1
+echo "instalacion de python"
+sleep 1 
+if command -v python3 &> /dev/null ; then
+    echo "python instalado"
+else
+    sudo apt update &&  sudo apt -y install python3
+    if [[ $? -ne 0 ]]; then
+        echo "error al instalar python"
+        exit 1
+    fi
+
+fi
 echo "build completado exitosamente"
