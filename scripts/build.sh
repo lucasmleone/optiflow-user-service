c8c0e7dccf943251265865373b4c617979cc470a
Lucas Leone <lucasmleone@gmail.com>
2025-04-02 18:33:31 -0300

    update pytest and pip

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,4 +13,20 @@ else
     fi
 
 fi
+
+if ! command -v pip3 &> /dev/null ; then
+    echo "pip3 no está instalado. Instalando..."
+    sudo apt update && sudo apt -y install python3-pip
+fi
+
+if command -v pytest &> /dev/null ; then
+    echo "pytest instalado"
+else
+    pip install pytest
+    if [[ $? -ne 0 ]]; then
+        echo "error al instalar pytest"
+        exit 1
+    fi
+
+fi
 echo "build completado exitosamente"
